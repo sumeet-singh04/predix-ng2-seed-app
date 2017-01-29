bc62df6f7fc4e6a537a86b5e77e43f326c849dfe
sumeet-singh04 <sumeet_singh871@yahoo.com>
2017-01-29 00:01:04 -0600

    Added application navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  menuItems:any = [{
+    "path": "tab1",
+    "icon": "fa-exclamation-triangle",
+    "eventName": "firstItem",
+    "label": "Alerts",
+    "subitems": [{"label": "Sub Category 1"}, {
+      "label": "Sub Category 2",
+      "eventName": "subitemTwo"
+    }, {"label": "Sub Category 3", "path": "subitem3"}]
+  }, {
+    "path": "tab2",
+    "icon": "fa-briefcase",
+    "label": "Cases",
+    "subitems": [{"label": "Sub Category 1"}, {"label": "Sub Category 2"}, {"label": "Sub Category 3"}]
+  }, {"path": "tab3", "icon": "fa-bar-chart", "label": "Analysis"}, {
+    "path": "tab4",
+    "icon": "fa-tachometer",
+    "label": "Dashboards"
+  }];
 }
